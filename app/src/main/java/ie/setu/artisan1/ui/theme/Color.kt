d72ae058698dd98fa84664077dc2b1588d047356
emily172 <20092335@mail.wit.ie>
2025-02-28 12:56:56 +0000

    Added colours for the light and dark theme.

--- app/src/main/java/ie/setu/artisan1/ui/theme/Color.kt
+++ app/src/main/java/ie/setu/artisan1/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package ie.setu.artisan1.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryRose = Color(0xFFE91E63)
+val SecondarySky = Color(0xFF64B5F6)
+val TertiarySoft = Color(0xFFF5F5F5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val PrimaryDRose = Color(0xFFC2185B)
+val SecondaryDSky = Color(0xFF42A5F5)
+val TertiaryBlack = Color(0xFF121212)
