4c25786740df738d8da85bfb0e85af08f6136ec6
emily172 <20092335@mail.wit.ie>
2025-03-02 14:45:34 +0000

    Updating the MainActivity for intergration.

--- app/src/main/java/ie/setu/artisan1/MainActivity.kt
+++ app/src/main/java/ie/setu/artisan1/MainActivity.kt
@@ -3,20 +3,37 @@ package ie.setu.artisan1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ie.setu.artisan1.data.ArtisanModel
+import ie.setu.artisan1.ui.components.general.MenuItem
 import ie.setu.artisan1.ui.screens.ScreenItem
+import ie.setu.artisan1.ui.screens.ScreenReport
 import ie.setu.artisan1.ui.theme.Artisan1Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,24 +46,76 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArtisanApp(modifier = Modifier)
+                    ArtisanApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ArtisanApp(modifier: Modifier = Modifier) {
+fun ArtisanApp() {
     val items = remember { mutableStateListOf<ArtisanModel>() }
+    var selectedMenuItem by remember { mutableStateOf(MenuItem.Item) }
 
-    ScreenItem(modifier = modifier, items = items)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                actions = {
+                    if (selectedMenuItem == MenuItem.Item) {
+                        IconButton(onClick = {
+                            selectedMenuItem = MenuItem.Record
+                        }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.List,
+                                contentDescription = "Options",
+                                tint = Color.White,
+                                modifier = Modifier.size(40.dp)
+                            )
+                        }
+                    } else {
+                        IconButton(onClick = {
+                            selectedMenuItem = MenuItem.Item
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = "Options",
+                                tint = Color.White,
+                                modifier = Modifier.size(40.dp)
+                            )
+                        }
+                    }
+                }
+            )
+        },
+        content = { paddingValues ->
+            Column(modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+            ) {
+                when (selectedMenuItem) {
+                    MenuItem.Item -> ScreenItem(modifier = Modifier, items = items)
+                    MenuItem.Record -> ScreenReport(modifier = Modifier, items = items)
+                }
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MyAppPreview() {
     Artisan1Theme {
-        ArtisanApp(modifier = Modifier)
+        ArtisanApp()
     }
 }
