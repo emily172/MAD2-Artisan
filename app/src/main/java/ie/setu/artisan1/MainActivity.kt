bfffec41ccfbc4dde19af95b244ca5578fffb749
emily172 <20092335@mail.wit.ie>
2025-03-02 21:52:11 +0000

    Getting item to record list nav working.

--- app/src/main/java/ie/setu/artisan1/MainActivity.kt
+++ app/src/main/java/ie/setu/artisan1/MainActivity.kt
@@ -3,22 +3,11 @@ package ie.setu.artisan1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -26,14 +15,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import ie.setu.artisan1.data.ArtisanModel
+import ie.setu.artisan1.navigation.NavHostProvider
+import ie.setu.artisan1.navigation.Record
+import ie.setu.artisan1.navigation.allDestinations
+import ie.setu.artisan1.ui.components.general.BottomAppBarProvider
 import ie.setu.artisan1.ui.components.general.MenuItem
-import ie.setu.artisan1.ui.screens.ScreenItem
-import ie.setu.artisan1.ui.screens.ScreenReport
+import ie.setu.artisan1.ui.components.general.TopAppBarProvider
 import ie.setu.artisan1.ui.theme.Artisan1Theme
 
 class MainActivity : ComponentActivity() {
@@ -55,59 +47,38 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ArtisanApp() {
+fun ArtisanApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
     val items = remember { mutableStateListOf<ArtisanModel>() }
-    var selectedMenuItem by remember { mutableStateOf(MenuItem.Item) }
+    var selectedMenuItem by remember { mutableStateOf<MenuItem?>(MenuItem.Item) }
+    val currentNavBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = currentNavBackStackEntry?.destination
+    val currentBottomScreen =
+        allDestinations.find { it.route == currentDestination?.route } ?: Record
 
     Scaffold(
+        modifier = modifier,
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.app_name),
-                        color = Color.White
-                    )
-                },
-                colors = TopAppBarDefaults.largeTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary
-                ),
-                actions = {
-                    if (selectedMenuItem == MenuItem.Item) {
-                        IconButton(onClick = {
-                            selectedMenuItem = MenuItem.Record
-                        }) {
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Filled.List,
-                                contentDescription = "Options",
-                                tint = Color.White,
-                                modifier = Modifier.size(40.dp)
-                            )
-                        }
-                    } else {
-                        IconButton(onClick = {
-                            selectedMenuItem = MenuItem.Item
-                        }) {
-                            Icon(
-                                imageVector = Icons.Filled.Add,
-                                contentDescription = "Options",
-                                tint = Color.White,
-                                modifier = Modifier.size(40.dp)
-                            )
-                        }
-                    }
-                }
-            )
+            TopAppBarProvider(
+                currentScreen = currentBottomScreen,
+                canNavigateBack = navController.previousBackStackEntry != null
+            ) { navController.navigateUp() }
         },
         content = { paddingValues ->
-            Column(modifier = Modifier
-                .padding(paddingValues)
-                .padding(16.dp)
-            ) {
-                when (selectedMenuItem) {
-                    MenuItem.Item -> ScreenItem(modifier = Modifier, items = items)
-                    MenuItem.Record -> ScreenReport(modifier = Modifier, items = items)
-                }
-            }
+            NavHostProvider(
+                modifier = modifier,
+                navController = navController,
+                paddingValues = paddingValues,
+                items = items
+            )
+        },
+        bottomBar = {
+            BottomAppBarProvider(
+                navController = navController,
+                currentScreen = currentBottomScreen,
+            )
         }
     )
 }
---
+++ app/src/main/java/ie/setu/artisan1/ui/components/general/TopAppBarProvider.kt
@@ -0,0 +1,80 @@
+package ie.setu.artisan1.ui.components.general
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ie.setu.artisan1.navigation.AppDestination
+import ie.setu.artisan1.navigation.Item
+import ie.setu.artisan1.ui.theme.Artisan1Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarProvider(
+    currentScreen: AppDestination,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = currentScreen.label,
+                color = Color.White
+            )
+        },
+        colors = TopAppBarDefaults.largeTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back Button",
+                        tint = Color.White,
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+            } else {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu Button",
+                    tint = Color.White,
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+        },
+        actions = { DropDownMenu() }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDownMenu() {
+    // Implement your DropDownMenu here
+    Text("Menu")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarPreview() {
+    Artisan1Theme {
+        TopAppBarProvider(
+            currentScreen = Item,
+            canNavigateBack = true,
+            navigateUp = {}
+        )
+    }
+}
